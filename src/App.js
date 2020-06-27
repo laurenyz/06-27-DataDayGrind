ccb9071591c4e73a6714b2ef13adf4d2f85a3cb8
laurenyz <laurenkyu@gmail.com>
2020-06-27 12:07:11 -0400

    added regression component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 import Board from './components/MVPGraph/Board.component';
 
+import Regression from './components/MVPGraph/Regression.component'
+
 function App() {
 	let url = 'https://covidtracking.com/api/v1/us/daily.json';
 
 	return (
 		<div className="App">
 			<Board url={url} />
+			<Regression />
 		</div>
 	);
 }
