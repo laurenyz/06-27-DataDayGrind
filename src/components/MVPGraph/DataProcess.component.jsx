77b551b38d99b8db3850a364888fae1f026c8611
Wesley Chen <weschen.github@gmail.com>
2020-06-27 13:04:28 -0400

    Fixed Date

--- src/components/MVPGraph/DataProcess.component.jsx
+++ src/components/MVPGraph/DataProcess.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import Regression from './Regression.component'
+import Regression from './Regression.component';
 
 function DataProcess({ data }) {
 	console.log(data);
-	const mappedData = data.map(dataObj => [dataObj.date-20200303, dataObj.positive])
-	return <div>
-		Data is loaded in console
-		<Regression mappedData={mappedData}/>
-		</div>;
+	const mappedData = data.map((dataObj) => {
+		let date = new Date(dataObj.dateChecked);
+		function daysIntoYear(date) {
+			return (
+				(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()) - Date.UTC(date.getFullYear(), 0, 0)) /
+				24 /
+				60 /
+				60 /
+				1000
+			);
+		}
+		let positiveCases = dataObj.positive;
+		return [ daysIntoYear(date) + 1, positiveCases ];
+	});
+	console.log('Mapped Data', mappedData);
+	return (
+		<div>
+			Data is loaded in console
+			<Regression mappedData={mappedData} />
+		</div>
+	);
 }
 
 export default DataProcess;
