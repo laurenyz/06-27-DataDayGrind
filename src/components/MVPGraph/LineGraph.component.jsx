763225c7f22a3d409875766811f4d9fd5a8886fd
matteoaricci <matteo.a.ricci@gmail.com>
2020-06-27 14:15:18 -0400

    Graph Looks Nicer

--- src/components/MVPGraph/LineGraph.component.jsx
+++ src/components/MVPGraph/LineGraph.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { VictoryChart, VictoryZoomContainer, VictoryLine, VictoryAxis, VictoryLabel } from 'victory';
+import { VictoryChart, VictoryZoomContainer, VictoryLine, VictoryAxis, VictoryLabel, VictoryVoronoiContainer, VictoryTooltip } from 'victory';
 
 
 class LineGraph extends Component {
@@ -24,26 +24,35 @@ class LineGraph extends Component {
         return (
             <div>
                 <VictoryChart 
-                width={2000}
+                width={1000}
                 height={500}
                 style={{parent: {marginLeft: '2em'}}}
-                // scale={{x: 'time'}}
                 containerComponent={
-                    <VictoryZoomContainer 
-                    responsive={false}
-                    zoomDimension='x'
-                    zoomDomain={this.state.zoomDomain}
-                    onZoomDomainChange={this.handleZoom.bind(this)}
-                    />
+                    // <VictoryZoomContainer 
+                    // responsive={false}
+                    // zoomDimension='x'
+                    // zoomDomain={this.state.zoomDomain}
+                    // onZoomDomainChange={this.handleZoom.bind(this)}
+					// />,
+					<VictoryVoronoiContainer 
+					voronoiDimension='x'
+					labels={({datum}) => `${datum.childName}: ${datum.y}`}
+					labelComponent={<VictoryTooltip
+					cornerRadius={0}
+					flyoutStyle={{fill: 'white'}}
+					/>}
+					/>
                 }
                 >
                 <VictoryLine 
-                style={{fill: 'tomato'}}
+				style={{data: {stroke: 'blue'}, labels: {fill: 'blue'}}}
+				name={'Actual'}
                 data={this.props.originalData}
                 />
 				<VictoryLine 
 				data={this.props.predictedData}
-				style={{fill: {color: 'red'}}}
+				name={'Predicted'}
+				style={{data: {strokeDasharray: '1em', strokeOpacity: 0.9}}}
 				/>
                 <VictoryAxis dependentAxis
                 label="Number of Confirmed Cases (x 10,000)"
