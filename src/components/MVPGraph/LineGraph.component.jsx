f2c8d15554692e7361515a949caff2aa0c41855d
Wesley Chen <weschen.github@gmail.com>
2020-06-27 15:21:39 -0400

    Changed Linegraph to functional component

--- src/components/MVPGraph/LineGraph.component.jsx
+++ src/components/MVPGraph/LineGraph.component.jsx
@@ -1,72 +1,68 @@
-import React, { Component } from 'react';
-import { VictoryBar, VictoryScatter, VictoryChart, VictoryZoomContainer, VictoryLine, VictoryAxis, VictoryLabel, VictoryVoronoiContainer, VictoryTooltip } from 'victory';
+import React, { useState } from 'react';
+import {
+	VictoryBar,
+	VictoryScatter,
+	VictoryChart,
+	VictoryZoomContainer,
+	VictoryLine,
+	VictoryAxis,
+	VictoryLabel,
+	VictoryVoronoiContainer,
+	VictoryTooltip
+} from 'victory';
 
+function LineGraph({ originalData, predictedData }) {
+	const [ selectedDomain, selectedDomainSet ] = useState(undefined);
+	const [ zoomDomain, zoomDomainSet ] = useState(undefined);
 
-class LineGraph extends Component {
-	constructor() {
-        super();
-        this.state= {}
-    }
-    
-    handleZoom(domain) {
-        this.setState({
-            selectedDomain: domain
-        })
-    }
+	const handleZoom = (domain) => {
+		selectedDomainSet(domain);
+	};
 
-    handleBrush(domain) {
-        this.setState({
-            zoomDomain: domain
-        })
-    }
+	const handleBrush = (domain) => {
+		zoomDomainSet(domain);
+	};
 
-    render() {
-        return (
-            <div>
-                <VictoryChart 
-                width={1000}
-                height={500}
-                style={{parent: {marginLeft: '2em'}}}
-                containerComponent={
-                    // <VictoryZoomContainer 
-                    // responsive={false}
-                    // zoomDimension='x'
-                    // zoomDomain={this.state.zoomDomain}
-                    // onZoomDomainChange={this.handleZoom.bind(this)}
+	return (
+		<div>
+			<VictoryChart
+				width={1000}
+				height={500}
+				style={{ parent: { marginLeft: '2em' } }}
+				containerComponent={
+					// <VictoryZoomContainer
+					// responsive={false}
+					// zoomDimension='x'
+					// zoomDomain={zoomDomain}
+					// onZoomDomainChange={handleZoom}
 					// />,
-					<VictoryVoronoiContainer 
-					voronoiDimension='x'
-					labels={({datum}) => `${datum.childName}: ${Math.floor(datum.y * 10000)}`}
-					labelComponent={<VictoryTooltip
-					cornerRadius={0}
-					flyoutStyle={{fill: 'white'}}
-					/>}
+					<VictoryVoronoiContainer
+						voronoiDimension="x"
+						labels={({ datum }) => `${datum.childName}: ${Math.floor(datum.y * 10000)}`}
+						labelComponent={<VictoryTooltip cornerRadius={0} flyoutStyle={{ fill: 'white' }} />}
 					/>
-                }
-                >
-                <VictoryBar
-				size={2}
-				style={{data: {fill: 'blue'}, labels: {fill: 'blue'}}}
-				name={'Actual'}
-                data={this.props.originalData}
-                />
-				<VictoryLine 
-				data={this.props.predictedData}
-				name={'Predicted'}
-				style={{data: {strokeDasharray: '1em', strokeOpacity: 0.9}}}
+				}
+			>
+				<VictoryBar
+					size={2}
+					style={{ data: { fill: 'blue' }, labels: { fill: 'blue' } }}
+					name={'Actual'}
+					data={originalData}
 				/>
-                <VictoryAxis dependentAxis
-                label="Number of Confirmed Cases (x 10,000)"
-                axisLabelComponent={<VictoryLabel dy={-12}/>}
-                />
-                <VictoryAxis 
-                label='Day'
-                />
-                </VictoryChart>
-            </div>
-        );
-    }
+				<VictoryLine
+					data={predictedData}
+					name={'Predicted'}
+					style={{ data: { strokeDasharray: '1em', strokeOpacity: 0.9 } }}
+				/>
+				<VictoryAxis
+					dependentAxis
+					label="Number of Confirmed Cases (x 10,000)"
+					axisLabelComponent={<VictoryLabel dy={-12} />}
+				/>
+				<VictoryAxis label="Day" />
+			</VictoryChart>
+		</div>
+	);
 }
 
 export default LineGraph;
-
