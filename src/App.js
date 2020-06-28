720917d4d15632bef0185b0366f7a58adabd3cab
matteoaricci <matteo.a.ricci@gmail.com>
2020-06-27 20:38:40 -0400

    added font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, createMuiTheme } from "@material-ui/core/styles"
 
 import './App.css';
 import Board from './components/MVPGraph/Board.component';
